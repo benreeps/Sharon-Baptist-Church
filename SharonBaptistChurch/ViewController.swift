0e4eab7566f6c4e2c56817159fd589c35aa1ae06
Ben Reeps <benreeps@gmail.com>
2021-02-12 13:12:23 -0500

    initial xcode project creation

---
+++ SharonBaptistChurch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SharonBaptistChurch
+//
+//  Created by Benjamin Reeps on 2/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
