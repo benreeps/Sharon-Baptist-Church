1c3ee44a33f8e274d206318c5de4f69f7fad5f58
Ben Reeps <benreeps@gmail.com>
2021-02-17 14:20:27 -0500

    create constants template file

---
+++ SharonBaptistChurch/Models/ConstantsTemplate.swift
@@ -0,0 +1,18 @@
+//
+//  ConstantsTemplate.swift
+//  SharonBaptistChurch
+//
+//  Created by Benjamin Reeps on 2/17/21.
+//
+
+import Foundation
+
+/*
+ struct Constants {
+     
+     static var API_KEY = ""
+     static var PLAYLIST_ID = ""
+     static var API_URL = ""
+     
+ }
+*/
