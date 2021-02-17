cc9bfdf153a30dc71d5bfb56a20cf9f84f67121e
Ben Reeps <benreeps@gmail.com>
2021-02-16 21:13:21 -0500

    add code to parse JSON response

--- SharonBaptistChurch/Models/Constants.swift
+++ SharonBaptistChurch/Models/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Constants {
     
-    static var API_KEY = "AIzaSyCBTSFagdYePVn49O4Z9po6zk3rKuXoSC8"
+    static var API_KEY = ""
     static var PLAYLIST_ID = "PL9qOK5nqpTFvl6SNaZVxjXcflLe0nAJPm"
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     
--- SharonBaptistChurch/Models/Model.swift
+++ SharonBaptistChurch/Models/Model.swift
@@ -14,12 +14,38 @@ class Model {
         let url = URL(string: Constants.API_URL)
         guard url != nil else {return}
         
+        // Get a URLSession object
         let session = URLSession.shared
         
+        // Get a data task from the URLSession object
         let dataTask = session.dataTask(with: url!) { (data, response, error) in
+            
+            // Check for errors
             if error != nil || data == nil {
                 return
             }
+            
+            do {
+                // Parse the data into video objects
+                let decoder = JSONDecoder()
+                
+                // Ex. iso8601 format = 2020-05-29T17:32:50Z
+                decoder.dateDecodingStrategy = .iso8601
+                
+                // Specify the type of data you want to decode the data into
+                let response = try decoder.decode(Response.self, from: data!)
+                
+                dump(response)
+            }
+            catch {
+                
+                
+            }
+            
+            
+            
+            
+            
         }
         dataTask.resume() 
     }
