312083ba85ab11fb0901aa9232abd2227bf2aabf
Ben Reeps <benreeps@gmail.com>
2021-02-19 15:28:11 -0500

    format video cell date

--- SharonBaptistChurch/Views/VideoTableViewCell.swift
+++ SharonBaptistChurch/Views/VideoTableViewCell.swift
@@ -10,7 +10,11 @@ import UIKit
 class VideoTableViewCell: UITableViewCell {
     
     
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
     
+    var video:Video?
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -21,6 +25,21 @@ class VideoTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func setCell(_ v:Video) {
+        
+        self.video = v
+        
+        guard self.video != nil else {return}
+        
+        self.titleLabel.text = video?.title
+        
+        // Format the video.date into a string
+        let df = DateFormatter()
+        df.dateFormat = "EEEE, MMM d, yyyy"
+        self.dateLabel.text = df.string(from: video!.published)
+        
     }
 
 }
