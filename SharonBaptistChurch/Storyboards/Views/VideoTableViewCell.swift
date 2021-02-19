7b155b4e727c62478c7831c0cf6f94ff647b7124
Ben Reeps <benreeps@gmail.com>
2021-02-19 14:54:15 -0500

    add views group and video table view subclass

---
+++ SharonBaptistChurch/Storyboards/Views/VideoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  VideoTableViewCell.swift
+//  SharonBaptistChurch
+//
+//  Created by Benjamin Reeps on 2/19/21.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
