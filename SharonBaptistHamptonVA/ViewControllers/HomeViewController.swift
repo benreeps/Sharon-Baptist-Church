1741fe371d80f8a0a80e9280fb8886ee851025e8
Ben Reeps <benreeps@gmail.com>
2021-04-02 22:02:51 -0400

    set up UI for podcastview on home view controller

--- SharonBaptistHamptonVA/ViewControllers/HomeViewController.swift
+++ SharonBaptistHamptonVA/ViewControllers/HomeViewController.swift
@@ -8,11 +8,20 @@
 import Foundation
 import UIKit
 
+
 class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var ourTeamView: UIView!
+    @IBOutlet weak var ourVisionButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        ourVisionButton.layer.cornerRadius = 9
+        setTitleViewImage()
+    }
+    
+    func setTitleViewImage() {
         let image: UIImage = UIImage(named: "sharon cropped png3")!
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
         imageView.contentMode = .scaleAspectFit
@@ -21,10 +30,8 @@ class HomeViewController: UIViewController {
         self.navigationItem.titleView = imageView
         self.navigationController?.navigationBar.setTitleVerticalPositionAdjustment(-5.0, for: .default)
         
-        
-       
-    
     }
+    
     
 }
 
--- SharonBaptistHamptonVA/ViewControllers/LiveDetailViewController.swift
+++ SharonBaptistHamptonVA/ViewControllers/LiveDetailViewController.swift
@@ -10,8 +10,6 @@ import youtube_ios_player_helper
 
 class LiveDetailViewController: UIViewController {
     
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var liveVideoPlayerView: YTPlayerView!
     
     
@@ -23,31 +21,17 @@ class LiveDetailViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        
-        // Clear the field
-        titleLabel.text = ""
-        dateLabel.text = ""
+    
         
         // Check if there is a video
         guard liveVideo != nil else {return}
         
         // Create the embed url
-        let embedUrlString = Constants.YT_EMBED_URL + liveVideo!.videoId
+       // let embedUrlString = Constants.YT_EMBED_URL + liveVideo!.videoId
         
-        self.liveVideoPlayerView.loadVideo(byURL: embedUrlString, startSeconds: 0)
+        self.liveVideoPlayerView.load(withPlaylistId: liveVideo!.videoId)
+       // self.liveVideoPlayerView.loadVideo(byURL: embedUrlString, startSeconds: 0)
         
-        // Set the title
-        titleLabel.text = liveVideo!.title
-        
-        // Set the date
-        updateVideoDateFormat(date: liveVideo!.published)
-        
-        // Set the description
-        self.liveVideoPlayerView.load(withVideoId: liveVideo!.videoId)
     }
-    
 
-    func updateVideoDateFormat(date: Date) {
-        self.dateLabel.text = DateManager.videoDateFormatter.string(from: date)
-    }
 }
